56ab265e5286abce93262b1baf2f92c2ad52cae3
Amir Reza Mehrbakhsh <amirreza@dakyaco.com>
2021-01-06 21:35:02 +0330

    Global route guards

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from "vue-router";
 import EventCreate from "../views/EventCreate.vue";
 import EventList from "../views/EventList.vue";
 import EventShow from "../views/EventShow.vue";
+import NProgress from "nprogress";
+import store from '@/store/index';
 
 Vue.use(VueRouter);
 
@@ -21,11 +23,27 @@ const routes = [
     path: '/events/:id/',
     name: 'events.show',
     component: EventShow,
-    props: true
+    props: true,
+    beforeEnter(routeTo, routeFrom, next) {
+      store.dispatch('event/fetchEvent', routeTo.params.id).then( () => {
+        next();
+      });
+    }
   }
 ];
 
-export default new VueRouter({
+const vueRouter = new VueRouter({
   mode: 'history',
   routes
 });
+
+vueRouter.beforeEach( (routeTo, routeFrom, next) => {
+  NProgress.start();
+  next();
+});
+
+vueRouter.afterEach( () => {
+  NProgress.done();
+});
+
+export default vueRouter;
